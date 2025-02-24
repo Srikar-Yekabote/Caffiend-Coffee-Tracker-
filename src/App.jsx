9041931dc057f772497a14676904a8682dfb6153
srikaryekabote <srikaryekabote20040301@gmail.com>
2025-02-24 21:38:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-// import React from 'react'
+import React from 'react'
 import Layout from './components/layout'
 import Hero from './components/Hero'
 import CoffeeFrom from './components/coffeeFrom'
 import Stats from './components/Stats'
 import History from './components/History'
 import { userAuth } from './context/AuthContext'
-import { coffeeConsumptionHistory } from './utils'
 
 
 
